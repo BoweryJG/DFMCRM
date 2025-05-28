6b429c952da6124831a005bf40879e0b02f76cd2
Jason Golden <jasonwilliamgolden@gmail.com>
2025-05-27 20:06:03 -0400

    chore: relax tsconfig and stub MUI types

---
+++ src/mui-overrides.d.ts
@@ -0,0 +1,11 @@
+declare module '@mui/material/Grid' {
+  import { ComponentType } from 'react';
+  const Grid: ComponentType<any>;
+  export default Grid;
+}
+
+declare module '@mui/material/ListItem' {
+  import { ComponentType } from 'react';
+  const ListItem: ComponentType<any>;
+  export default ListItem;
+}
